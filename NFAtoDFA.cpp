cafe0183457830eecfaf1a6aa8d4ab625d4d1add
sky10w <sky10w@163.com>
2024-01-10 12:39:02 +0800

    111

--- NFAtoDFA.cpp
+++ NFAtoDFA.cpp
@@ -1,8 +1,8 @@
 #include "NFAtoDFA.hpp"
 
-bool Insertf( vector<DFAState>S, Map::iterator_set &I )
+bool Insertf( vector<DFAState>S, Map::iterator_set& I )
 {
-    if(!I.size()) return false;
+    if (!I.size()) return false;
     for (auto k : S)
     {
         if (k.StateNtoD == I)
@@ -13,15 +13,15 @@ bool Insertf( vector<DFAState>S, Map::iterator_set &I )
     return true;
 }
 
-DFAState Move( DFAState &S, val_t edge )
+DFAState Move( DFAState& S, val_t edge )
 {
     //std::cout<<S.StateNtoD.size()<<endl;
     DFAState result;
-    for (auto &k : S.StateNtoD)
+    for (auto& k : S.StateNtoD)
     {
-        auto x = k.next(edge);//遍历一个状态集内的所有状态
-        
-        for (auto &i : x)
+        auto x = k.next( edge );//遍历一个状态集内的所有状态
+
+        for (auto& i : x)
         {
             result.StateNtoD.insert( i );//将状态全部输入dfa中
         }
@@ -30,20 +30,20 @@ DFAState Move( DFAState &S, val_t edge )
     return result;
 }
 vector<DFAState>Set;
-bool Recursion( DFAState State,ato::Map& DFA )
+void Recursion( DFAState State, ato::Map& DFA )
 {
-    cout<<DFA.all().size()<<endl;
+    cout << DFA.all().size() << endl;
     //State为输入状态，Set为整个状态集的集合，DFA为要返回的DFA
     //int _bool1 = 0;
     //int _bool2 = 0;
     DFAState dfa1 = Move( State, '0' );
     DFAState dfa2 = Move( State, '1' );
-    int a1=dfa1.StateNtoD.size(),a2=dfa2.StateNtoD.size();
+    int a1 = dfa1.StateNtoD.size(), a2 = dfa2.StateNtoD.size();
     bool st1 = Insertf( Set, dfa1.StateNtoD );
-    
+
     if (st1 == true)
     {
-        
+
         int k = 0;//记录节点状态
         for (auto i : dfa1.StateNtoD)
         {
@@ -61,14 +61,14 @@ bool Recursion( DFAState State,ato::Map& DFA )
         }
         Set.push_back( dfa1 );
         DFA.insertEdge( State.SetNode, dfa1.SetNode, '0' );
-         Recursion( dfa1, DFA );
+        Recursion( dfa1, DFA );
     } else if (st1 == false)
     {
         for (auto m : Set)
         {
             if (dfa1.StateNtoD == m.StateNtoD)
             {
-                dfa1=m;
+                dfa1 = m;
                 DFA.insertEdge( State.SetNode, dfa1.SetNode, '0' );
             }
             //_bool1 = 1;//出现循环构建循环边，并返回，递归完成
@@ -77,7 +77,7 @@ bool Recursion( DFAState State,ato::Map& DFA )
     bool nd1 = Insertf( Set, dfa2.StateNtoD );//对不同输入求状态然后判断是否加入新状态
     if (nd1 == true)
     {
-        
+
         int k = 0;
         for (auto i : dfa2.StateNtoD)
         {
@@ -102,13 +102,13 @@ bool Recursion( DFAState State,ato::Map& DFA )
         {
             if (dfa2.StateNtoD == m.StateNtoD)
             {
-                dfa2=m;
+                dfa2 = m;
                 DFA.insertEdge( State.SetNode, dfa2.SetNode, '1' );
             }
             //_bool2 = 1;
         }
     }
-//从新加入的状态及其对应节点开始递归
+    //从新加入的状态及其对应节点开始递归
 }
 
 
@@ -117,18 +117,18 @@ ato::Map NtoD( ato::Map& _NFA )
     ato::Map DFA;
 
     DFAState q0;
-    
+
     //std::vector<DFAState>StateSet;//记录状态集
-    for (auto &i : _NFA.all())
+    for (auto& i : _NFA.all())
     {
         if (i.type() == node_t::START)
         {
             q0.StateNtoD.insert( i );//将初始状态存入初始状态集中
-            q0.SetNode = DFA.insertNode(node_t::START);//定位首节点
+            q0.SetNode = DFA.insertNode( node_t::START );//定位首节点
         }
     }
-    Set.push_back(q0);
-    cout<<DFA.all().size()<<endl;
+    Set.push_back( q0 );
+    cout << DFA.all().size() << endl;
     Recursion( q0, DFA );
     return DFA;
 }
